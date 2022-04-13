7b66c52a8eb82f2a2595d92d7df8d24cfc0a7cc0
j-lyttle <lyttle.jayden@gmail.com>
2022-04-13 16:36:15 +1000

    Creation of initial maze methods

--- MazeClasses/Maze.java
+++ MazeClasses/Maze.java
@@ -1,5 +1,27 @@
 package MazeClasses;
 
 public class Maze{
+    private String Author;
+    private String Name;
+    private Tile[][] Maze;
 
+
+    public Maze(String Author, String Name, int[] size){
+        this.Author = Author;
+        this.Name = Name;
+
+        if(size.length != 2){
+            //Error Throwing
+        }else{
+            this.Maze = new Tile[size[1]][size[2]];
+        }
+    }
+
+    public void setStart(int x, int y){
+
+    }
+
+    public void setEnd(int x, int y){
+
+    }
 }

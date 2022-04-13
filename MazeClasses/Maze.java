a8ae433846750c8e5633048268a393908b8feae9
j-lyttle <lyttle.jayden@gmail.com>
2022-04-13 16:23:54 +1000

    Create intial classes + initial UML
    Create the Tile + Maze classes, add the UML file to be updated as the project grows, reformate the drive

---
+++ MazeClasses/Maze.java
@@ -0,0 +1,5 @@
+package MazeClasses;
+
+public class Maze{
+
+}
---
+++ MazeClasses/Tile.java
@@ -0,0 +1,11 @@
+package MazeClasses;
+
+public class Tile {
+    private boolean leftWall;
+    private boolean rightWall;
+    private boolean topWall;
+    private boolean bottomWall;
+
+    public Tile(){}
+
+}

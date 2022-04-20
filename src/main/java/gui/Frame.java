0d35434962d7bccf9474a0c718beba4d67542ad3
j-lyttle <lyttle.jayden@gmail.com>
2022-04-20 14:21:19 +1000

    Create Tiles
    Turns each individual button into a cell with 4 border buttons (noting that location x,y is probably broken now)

--- src/main/java/gui/Frame.java
+++ src/main/java/gui/Frame.java
@@ -3,6 +3,7 @@ package src.main.java.gui;
 import src.main.java.maze.core.Maze;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Frame {
     int[] mazeSize = new int[2];
@@ -23,7 +24,7 @@ public class Frame {
         // make the program process close when the main window is closed
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        initialise("","");
+        initialise("2","2");
 
 
         // this is where "user customization" will come in
@@ -72,20 +73,55 @@ public class Frame {
             for (int y = 0; y < myMaze.mazeSize()[1]; y++) {
                 // border styling
                 JButton tempBTN = new JButton("");
-
-                tempBTN.setBounds(x * 50, 75 + y * 50, 50, 50);
+                tempBTN.setBounds(10 + x * 50, 75 + y * 50, 40, 10);
                 int finalX = x;
                 int finalY = y;
-                tempBTN.addActionListener(action -> mazeButtonPressed(finalX,finalY));
+                tempBTN.addActionListener(action -> mazeButtonPressed(finalX,finalY, tempBTN));
+                tempBTN.setBackground(Color.WHITE);
                 window.add(tempBTN);
+
+                JButton tempBTN2 = new JButton("");
+                tempBTN2.setBounds(x * 50, 10+75 + y * 50, 10, 40);
+                tempBTN2.addActionListener(action -> mazeButtonPressed(finalX,finalY, tempBTN2));
+                tempBTN2.setBackground(Color.WHITE);
+                window.add(tempBTN2);
             }
         }
+
+        for(int i = 0; i < myMaze.mazeSize()[1]; i++){
+            JButton tempBTN2 = new JButton("");
+            tempBTN2.setBounds(myMaze.mazeSize()[0] * 50, 10+75 + i * 50, 10, 40);
+            int finalX = myMaze.mazeSize()[0];
+            int finalY = i;
+            tempBTN2.addActionListener(action -> mazeButtonPressed(finalX,finalY, tempBTN2));
+            tempBTN2.setBackground(Color.WHITE);
+            window.add(tempBTN2);
+        }
+
+        for(int i = 0; i < myMaze.mazeSize()[0]; i++){
+            JButton tempBTN = new JButton("");
+            tempBTN.setBounds(10 + i * 50, 75 + myMaze.mazeSize()[1] * 50, 40, 10);
+            int finalX = i;
+            int finalY = myMaze.mazeSize()[1];
+            tempBTN.addActionListener(action -> mazeButtonPressed(finalX,finalY, tempBTN));
+            tempBTN.setBackground(Color.WHITE);
+            window.add(tempBTN);
+        }
+
 
         SwingUtilities.updateComponentTreeUI(window);
     }
 
-    public void mazeButtonPressed(int x, int y){
+    public void mazeButtonPressed(int x, int y, JButton button){
+        if(button.getBackground() == Color.BLACK){
+            button.setBackground(Color.WHITE);
+        }else{
+            button.setBackground(Color.BLACK);
+        }
+
+
         //Prints the coords of the button pressed
         System.out.println(x + ", " + y);
+
     }
 }

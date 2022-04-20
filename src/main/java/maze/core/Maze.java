51a107d136f1df57a7b4f197de5face68e146400
Grathium-Industries <GrathiumIndustries@gmail.com>
2022-04-20 11:23:16 +1000

    working checkpoint of backend implementation of maze classess

--- src/main/java/maze/core/Maze.java
+++ src/main/java/maze/core/Maze.java
@@ -1,15 +1,18 @@
 package src.main.java.maze.core;
 
+import java.util.ArrayList;
+
 public class Maze{
     private String Author;
     private String Name;
-    private Tile[][] myMaze;
+    private Tile[][] myMaze = new Tile[60][60];
 
 
     public Maze(int[] size){
         for (int x = 0; x < size[0]; x++) {
             for (int y = 0; y < size[1]; y++) {
-                myMaze[x][y] = new Tile();
+                boolean[] myTileWalls = {false, false, false, false};
+                myMaze[x][y] = new Tile(myTileWalls);
             }
         }
     }

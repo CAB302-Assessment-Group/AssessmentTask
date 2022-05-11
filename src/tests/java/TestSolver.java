b3f8c6d97f6d6055b53de46d1f3dc18a89f1237e
JackFFFFFF <n10794948@qut.edu.au>
2022-05-11 11:05:17 +1000

    Adding some more tests again #38

--- src/tests/java/TestSolver.java
+++ src/tests/java/TestSolver.java
@@ -14,11 +14,15 @@ public class TestSolver {
      */
     @BeforeEach
     public void ConstructMaze(){
+        //TODO Add in call to automatically generate or manually make one so we can test solvability
         int[] size = {10,10};
         testMaze = new Maze(size, false);
         mySolver = new Solver();
     }
-
+    /**
+     * Test 1: Test to see if the neighbours functions returns values adjacent to the cell
+     * @author Hudson
+     */
     @Test
     public void TestNeighbours() {
         // make sure that the function returns a value
@@ -33,5 +37,20 @@ public class TestSolver {
 
         boolean[] tile_4_4N = {true, true, false, true};
         assertArrayEquals(tile_4_4N, mySolver.neighbours(4, 4, testMaze));
+    }
+    /**
+     * Test 2: Test to see if maze solver can solve the maze faster than a human (i.e. 60 seconds)
+     * #ID 4
+     * Stole timing stuff from https://stackoverflow.com/questions/180158/how-do-i-time-a-methods-execution-in-java
+     * @author Jack
+     */
+    @Test
+    public void TestSolverTime(){
+        long startTime = System.nanoTime();
+        //mySolver.solveMaze(testMaze);
+        long endTime = System.nanoTime();
+
+        long duration = (endTime - startTime)/1000000;  //divide by 1000000 to get milliseconds.
+        assertTrue(duration<60000,"Maze solver took longer then 60 seconds");
     }
 }

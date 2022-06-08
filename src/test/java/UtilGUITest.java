dcfd7a243ad36ba76356aa2106d95c78d3af0dcb
JackFFFFFF <n10794948@qut.edu.au>
2022-06-08 21:36:56 +1000

    Adding Unit tests to help with debugging new refactored methods.

--- src/test/java/UtilGUITest.java
+++ src/test/java/UtilGUITest.java
@@ -104,6 +104,42 @@ class UtilGUITest {
         //Invalid Case 5
         inputs[0] ="1F";inputs[1]="one";
         assertFalse(Util.validateInput(inputs),"Not valid inputs " + inputs);
+    }
 
+    /**
+     * Test 4
+     * Validate that generateBounds works with original code from Jayden
+     * @author Jack
+     */
+    @Test
+    void TestGenerateBounds(){
+        int xposition =4,x=2,yposition=3,y=2,wallWidth=10,wallLength=40,between_walls=wallLength+wallWidth;
+        double scale_factor = Util.scaleFactor(testMaze.largestDimension(),1440);
+
+        //Refactored method 1
+        int[] bounds1 = Util.generateBounds(xposition,x,between_walls,scale_factor,yposition,y,wallWidth,wallLength);
+        //Original code 1
+        int[] expected1 = {(int) Math.floor(xposition + x * between_walls * scale_factor), (int) Math.floor(10 * scale_factor+yposition + y * between_walls * scale_factor), (int) Math.floor(wallWidth * scale_factor), (int) Math.floor(wallLength * scale_factor)};
+        assertEquals(expected1[0],bounds1[0]);
+        assertEquals(expected1[1],bounds1[1]);
+
+        int[] bounds2 = Util.generateBounds(x,wallLength,wallWidth,y,scale_factor);
+        int[] expected2 = {(int) Math.floor(((x * (wallLength + wallWidth)) + (wallLength/4))*scale_factor), (int) Math.floor(((y * (wallLength + wallWidth)) + (wallLength/4))*scale_factor), (int) Math.floor(40 * scale_factor), (int) Math.floor(40 * scale_factor)};
+        assertEquals(expected2[0],bounds2[0]);
+        assertEquals(expected2[1],bounds2[1]);
+
+        for(int i =0;i<testMaze.mazeSize()[1];i++){
+            int[] bounds3 = Util.generateBoundsLoop(xposition, testMaze.mazeSize()[0], between_walls, scale_factor, yposition,i,wallLength,wallLength);
+            int[] expected3 = {(int) Math.floor(xposition + testMaze.mazeSize()[0] * between_walls * scale_factor), (int) Math.floor(10 * scale_factor+yposition + i * between_walls * scale_factor), (int) Math.floor(wallWidth * scale_factor), (int) Math.floor(wallLength * scale_factor)};
+            assertEquals(expected3[0],bounds3[0]);
+            assertEquals(expected3[1],bounds3[1]);
+        }
+
+        for(int i =0;i<testMaze.mazeSize()[0];i++){
+            int[] bounds4 = Util.generateBounds(scale_factor,xposition,i,between_walls,yposition,testMaze.mazeSize()[1],wallWidth,wallLength);
+            int[] expected4 = {(int) Math.floor(10 *scale_factor+xposition + i * between_walls * scale_factor), (int) Math.floor(yposition + testMaze.mazeSize()[1] * between_walls * scale_factor),(int) Math.floor( wallLength * scale_factor),(int) Math.floor( wallWidth * scale_factor)};
+            assertEquals(expected4[0],bounds4[0]);
+            assertEquals(expected4[1],bounds4[1]);
+        }
     }
 }
